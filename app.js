749e2ad65b239956c38967ae9dd54db12268ab38
ki_blank <lbin7310@gmail.com>
2019-08-01 11:26:07 +0900

    Add localMiddleware

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,12 +11,15 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug")
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+
+// local middleware를 통해서 global 변수를 접근한다.
+app.use(localMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
