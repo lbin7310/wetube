618700eef97108602a30d87bee56e2f982211cbb
ki_blank <lbin7310@gmail.com>
2019-08-01 21:12:58 +0900

    Modify from videos to home controller of routes.home

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import routes from "../routes";
-import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { videos, upload, videoDetail, editVideo, deleteVideo, home } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.home, videos)
-videoRouter.post(routes.upload, upload)
+videoRouter.get(routes.home, home)
+videoRouter.get(routes.upload, upload)
 videoRouter.get(routes.videoDetail, videoDetail)
-videoRouter.post(routes.editVideo, editVideo)
-videoRouter.post(routes.deleteVideo, deleteVideo)
+videoRouter.get(routes.editVideo, editVideo)
+videoRouter.get(routes.deleteVideo, deleteVideo)
 
 export default videoRouter;
