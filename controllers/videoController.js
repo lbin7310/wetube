95d01b24cc1f914b5a692cbaa3a37549af74b220
ki_blank <lbin7310@gmail.com>
2019-08-01 21:07:47 +0900

    Modify home, search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,22 +1,25 @@
 // Global
+import { videos } from "../db";
 
-export const home = (req, res) => 
-  res.render("Home", { pageTitle: "Home" });
+export const home = (req, res) => {
+  res.render("Home", { pageTitle: "Home", videos });
+
+}
 
 export const search = (req, res) => {
   const { query: {term: searchingBy} } = req;
-  res.render("Search", { pageTitle: "Search", searchingBy })
+  res.render("Search", { pageTitle: "Search", searchingBy, videos })
 };
 
 // Videos
 
-export const videos = (req, res) => 
-  res.render("Videos", { pageTitle: "Videos" });
+// export const videos = (req, res) => 
+//   res.render("Videos", { pageTitle: "Videos" });
 export const upload = (req, res) => 
-  res.render("Upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) => 
   res.render("Video Detail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => 
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => 
-  res.render("Delete Video", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
