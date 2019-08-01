4834bcf09f37c49aeb2ba1166037ec05531d695b
ki_blank <lbin7310@gmail.com>
2019-08-01 21:06:00 +0900

    Modify from login to getLogin,postLogin and from join to getJoin,postJoin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,32 @@
+import routes from "../routes";
+
 // Videos
 
-export const join = (req, res) => 
+export const getJoin = (req, res) => {
   res.render("Join", { pageTitle: "Join" });
-export const login = (req, res) => 
+}
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2}
+  } = req;
+  if (password !== password2) {
+    res.status(400)
+    res.render("Join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+}
+
+
+export const getLogin = (req, res) => {
   res.render("Login", { pageTitle: "Login" });
+}
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+}
+
 export const logout = (req, res) => 
   res.render("Logout", { pageTitle: "Logout" });
 
@@ -12,7 +35,7 @@ export const logout = (req, res) =>
 export const users = (req, res) => 
   res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) => 
-  res.render("User Detail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => 
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => 
