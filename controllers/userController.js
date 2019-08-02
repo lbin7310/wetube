5a5eefb494df3c08503825a5e4e7ead461f908b4
ki_blank <lbin7310@gmail.com>
2019-08-02 11:03:59 +0900

    Modify logout controller when logout to home page

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,8 +27,11 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 }
 
-export const logout = (req, res) => 
-  res.render("Logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+  // To Do: Process Log out
+  res.redirect(routes.home);
+}
+  
 
 // Users
 
