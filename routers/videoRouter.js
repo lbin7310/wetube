ea4b8e97b9bba0c5bd25c70f380ef77c41ebe7d6
ki_blank <lbin7310@gmail.com>
2019-08-03 15:31:51 +0900

    Split upload, video detail, edit video, delete video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { postUpload, getUpload, videoDetail, editVideo, deleteVideo, home } from "../controllers/videoController";
+import { postUpload, getUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo, home } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.home, home);
+// upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
