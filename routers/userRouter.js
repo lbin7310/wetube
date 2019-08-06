f1a17d3200f19269b53e6f097f038186717b0537
ki_blank <lbin7310@gmail.com>
2019-08-06 10:46:35 +0900

    Write userDetail

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword, users } from "../controllers/userController";
+import { userDetail, getEditProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 // userRouter.get(routes.home, users);
-userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.editProfile, getEditProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
